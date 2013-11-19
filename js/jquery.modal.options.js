2a1d3273e097d31d0e615233d3d325e2c1f9a263
jameslaws <jameslaws@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-19 20:38:41 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/ninja-forms-modal/trunk@807055 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jquery.modal.options.js
@@ -0,0 +1,68 @@
+jQuery( document ).ready(function($) {
+
+  var wheight = $(window).height();
+  var mheight = $(".nf-modal").outerHeight();
+
+  $('a.nf-modal-link').on( "click", function() {
+    $(this).modal({
+      closeText: '&times;',
+      fadeDuration: 250
+    });
+
+    return false;
+  });
+
+  if(mheight > wheight) {
+    var height = $(window).height() - 150;
+    $(".nf-modal").height(height);
+  }
+
+  $(document).on($.modal.OPEN, function(event, modal) {
+    $(document).off( 'mp_page_change.scroll' );
+    $('html').css('overflow', 'hidden');
+  });
+
+  $(document).on($.modal.CLOSE, function(event, modal) {
+    $('html').css('overflow', 'auto');
+      jQuery(document).on( 'mp_page_change.scroll', function( e, form_id, new_page, old_page ) {
+        ninja_forms_scroll_to_top( form_id );
+      });
+  });
+
+  $( window ).resize(function() {
+    ninja_forms_resize_modal();
+  });
+
+  $(document).on( 'mp_page_change.modal_resize', function( e, form_id, new_page, old_page ) {
+    ninja_forms_resize_modal();
+  });
+
+  $(document).on( 'ninja_forms_conditional_show', function(e) {
+    ninja_forms_resize_modal();
+  });  
+
+  $(document).on( 'ninja_forms_conditional_hide', function(e) {
+    ninja_forms_resize_modal();
+  });
+
+});
+
+function ninja_forms_resize_modal() {
+    var content = jQuery(".nf-modal").find(".nf-modal-content").find(".ninja-forms-form-wrap");
+    var page_height = jQuery(content).height();
+    var page_width = jQuery(content).width();
+
+    var window_height = jQuery(window).height();
+    var window_width = jQuery(window).width();
+
+    var modal_width = page_width;
+    var modal_height = page_height + 30;
+
+    if ( modal_height > ( window_height / 1.2 ) ) {
+      modal_height = window_height / 1.2;
+    }
+
+    jQuery(".nf-modal").height( modal_height );
+    jQuery(".nf-modal-content").css("padding", 15);
+    jQuery.modal.resize();    
+}
